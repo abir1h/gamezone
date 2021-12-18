4811f4fbfbd1aa4a6bf7c954031588160b48c73f
abir1h <abirrahman959@gmail.com>
2021-12-18 15:36:38 +0600

    initial commit

---
+++ lib/Screens/Appurl/Appurl.dart
@@ -0,0 +1,39 @@
+class AppUrl {
+  static const String liveBaseURL = "https://sportclubff.com/api/";
+  static const String pic_url1 = "https://sportclubff.com/";
+  static const String pic_url2 = "http://bestaid.codebuzzbd.com/public/";
+
+  static const String baseURL = liveBaseURL;
+  static const String login = baseURL + "login";
+  static const String reg = baseURL + "register";
+  static const String maintanence = baseURL + "maintain";
+  static const String apk = baseURL + "apk";
+  static const String otp = baseURL + "otp";
+  static const String slider = baseURL + "auth/show/slider";
+  static const String shop = baseURL + "auth/show/shop";
+  static const String logout = baseURL + "auth/logout";
+  static const String withdraw = baseURL + "auth/add/withdraw/money/request";
+  static const String deposite = baseURL + "auth/add/deposit/money/request";
+  static const String transaction_hostory = baseURL + "auth/show/transaction/history";
+  static const String gamelist = baseURL + "auth/show/game/list/";
+  static const String game_details = baseURL + "auth/show/game/details/";
+  static const String join_match = baseURL + "auth/add/game/player/";
+  static const String room_details = baseURL + "auth/show/game/room/player/";
+  static const String game_prize = baseURL + "auth/show/game/prize/";
+  static const String profile_update = baseURL + "auth/profile/update";
+  static const String change_pass = baseURL + "auth/change/password";
+  static const String result = baseURL + "auth/show/game/result/f";
+  static const String result_details = baseURL + "auth/show/game/result/details/";
+  static const String count = baseURL + "auth/show/count/dashboard";
+  static const String match_count = baseURL + "auth/show/count/match";
+  static const String forget = baseURL + "forget/password";
+  static const String forget_verify = baseURL + "forget/password/verify";
+  static const String my_referral = baseURL + "auth/show/count/promo/dashboard";
+  static const String statistics = baseURL + "auth/show/game/statistics";
+  static const String bkash = baseURL + "auth/show/wallet";
+  static const String withou_gamedetails = baseURL + "auth/show/game/room/player/alltime/";
+  static const String telegram = baseURL + "auth/show/telegram";
+  static const String complain = baseURL + "auth/show/complain";
+  static const String ongoin = baseURL + "auth/show/ongoing";
+
+}
